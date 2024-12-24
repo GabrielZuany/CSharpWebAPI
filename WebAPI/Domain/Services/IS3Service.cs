45aeffff55917d5c9b749950ffd994057b14ed7a
GabrielZuany <gzuanydev@gmail.com>
2024-12-24 15:17:51 -0300

    Update IS3Service.cs
    new methods

--- WebAPI/Domain/Services/IS3Service.cs
+++ WebAPI/Domain/Services/IS3Service.cs
@@ -1,11 +1,14 @@
-﻿using WebAPI.Domain.Enum;
+﻿using Amazon.S3.Model;
+using WebAPI.Domain.Enum;
 
 namespace WebAPI.Domain.Services
 {
     public interface IS3Service
     {
         Task<bool> CreateBucketAsync(string _bucketName);
-        Task ListBucketsAsync();
+        Task<List<string>> ListBucketsNameAsync();
+        Task<List<S3Bucket>> ListBucketsMetadataAsync();
+        Task<bool> BucketAlreadyExists(string _bucketName);
         Task<bool> PutObjectAsync(string bucketName, string objectName, string objectPath);
     }
 }
